50b0e0593d9600b378ea8238544cf25b6a9439c5
Andre Mercer <amercer1@email.arizona.edu>
2013-09-06 00:09:55 -0700

    Added Post Route

--- js/app.js
+++ js/app.js
@@ -1,13 +1,32 @@
 App = Ember.Application.create();
 
 App.Router.map(function(){
-  this.resource('posts');
+  this.resource('posts')
+  this.resource('post',{path: ':post_id'});
 });
 
 App.PostsRoute = Ember.Route.extend({
   model: function(){
     return posts;
   }
+});
+
+App.PostRoute = Ember.Route.extend({
+  model: function(params){
+    return posts.findBy('id', params.post_id);
+  }
+});
+
+//App.PostController = Ember.ObjectController.extend();
+
+Ember.Handlebars.helper('format-date', function(date){
+  return moment(date).fromNow();
+});
+
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.helper('format-markdown', function(input){
+  return new Handlebars.SafeString(showdown.makeHtml(input));
 });
 
 var posts = [{
